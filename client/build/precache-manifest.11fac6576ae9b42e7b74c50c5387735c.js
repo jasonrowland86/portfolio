b5394439f0648517a5b9eb06761254f0fcb72f20
Jason Rowland <jasonrowland@Jasons-MBP.attlocal.net>
2019-08-22 13:41:32 -0500

    removed build form git ignore

---
+++ client/build/precache-manifest.11fac6576ae9b42e7b74c50c5387735c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef7919bfe586946faca07f2243d4079",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d7ce70973b0444b4e40",
+    "url": "/static/css/main.4388c244.chunk.css"
+  },
+  {
+    "revision": "c74149412f18ba6ac39e",
+    "url": "/static/js/2.152c76fb.chunk.js"
+  },
+  {
+    "revision": "3d7ce70973b0444b4e40",
+    "url": "/static/js/main.73450224.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
